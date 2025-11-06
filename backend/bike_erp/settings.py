041fc2b483e7bbd8af2541631918a802aeb7a9d8
Ben Makhlouf Mohamed <48968433+hamouda95@users.noreply.github.com>
2025-11-06 15:07:13 +0100

    Update settings.py

--- backend/bike_erp/settings.py
+++ backend/bike_erp/settings.py
@@ -103,7 +103,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS Configuration
 #CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000').split(',')
-CORS_ALLOWED_ORIGINS = https://erp-micheldevelo-frontend.onrender.com
+CORS_ALLOWED_ORIGINS = config(
+    'CORS_ALLOWED_ORIGINS',
+    default='http://localhost:3000,http://127.0.0.1:3000'
+).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
 # REST Framework Configuration
