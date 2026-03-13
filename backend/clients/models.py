48bf77daac0d3cbdea3779173085cca2ad4d34ae
Ben Makhlouf Mohamed <48968433+hamouda95@users.noreply.github.com>
2026-03-13 16:41:16 +0100

    Add import statement for Django models

--- backend/clients/models.py
+++ backend/clients/models.py
@@ -1,3 +1,5 @@
+from django.db import models
+
 class Client(models.Model):
     first_name = models.CharField(max_length=100, blank=True, default='')
     last_name = models.CharField(max_length=100, blank=True, default='')
